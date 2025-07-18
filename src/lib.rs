937e8d5f14d23ee8ed5d09e4f52512a8457fe844
Olamide Omolola <omololaolamidex@gmail.com>
2025-07-18 06:14:19 +0200

    updated tests and interface

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@
 
 //! Implemtation for the kvs crate
 
-use std::collections::HashMap;
+use std::{collections::HashMap, io::Error, path, result};
+
+pub type Result<T> = result::Result<T, Error>;
 
 /// The store for kvs crate
 pub struct KvStore {
@@ -29,17 +31,21 @@ impl KvStore {
         }
     }
 
+    pub fn open(path: &path::Path) -> Result<Self> {
+        unimplemented!();
+    }
+
     /// ```
     /// # use kvs::KvStore;
     /// #
     /// # fn main() {
     /// # let mut store = KvStore::new();
     /// # store.set("name".to_string(), "olamide".to_string());
-    /// assert_eq!(store.get("name".to_string()), Some("olamide".to_string()));
+    /// assert_eq!(store.get("name".to_string())?, Some("olamide".to_string()));
     /// # }
     /// ```
-    pub fn get(&self, key: String) -> Option<String> {
-        return self.elements.get(&key).cloned();
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        return Ok(self.elements.get(&key).cloned());
     }
 
     /// ```
@@ -48,11 +54,12 @@ impl KvStore {
     /// # fn main() {
     /// # let mut store = KvStore::new();
     /// store.set("name".to_string(), "olamide".to_string());
-    /// assert_eq!(store.get("name".to_string()), Some("olamide".to_string()));
+    /// assert_eq!(store.get("name".to_string())?, Some("olamide".to_string()));
     /// # }
     /// ```
-    pub fn set(&mut self, key: String, value: String) {
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
         self.elements.insert(key, value);
+        Ok(())
     }
 
     /// ```
@@ -62,10 +69,11 @@ impl KvStore {
     /// # let mut store = KvStore::new();
     /// # store.set("name".to_string(), "olamide".to_string());
     /// store.remove("name".to_string());
-    /// # assert_eq!(store.get("name".to_string()), None);
+    /// # assert_eq!(store.get("name".to_string())?, None);
     /// # }
     /// ```
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.elements.remove(&key);
+        Ok(())
     }
 }
